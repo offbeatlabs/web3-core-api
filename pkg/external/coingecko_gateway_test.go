fd17332685a07c2549b35f0c3e8f465bbfea1cb5
Arham Jain <arham.nec@gmail.com>
2022-10-29 21:13:11 +0530

    test(ut): added ut for missing coingecko methods

--- pkg/external/coingecko_gateway_test.go
+++ pkg/external/coingecko_gateway_test.go
@@ -2,6 +2,7 @@ package external
 
 import (
 	httpClient "github.com/arhamj/offbeat-api/commons/http_client"
+	"github.com/arhamj/offbeat-api/commons/logger"
 	"testing"
 )
 
@@ -17,7 +18,9 @@ func TestCoingeckoGateway_GetTokenList(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := NewCoingeckoGateay(httpClient.NewHttpClient(true))
+			lgr := logger.NewAppLogger(logger.NewLoggerConfig("debug", true, "console"))
+			lgr.InitLogger()
+			c := NewCoingeckoGateway(lgr, httpClient.NewHttpClient(true))
 			got, err := c.GetTokenList()
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetTokenList() error = %v, wantErr %v", err, tt.wantErr)
@@ -67,7 +70,9 @@ func TestCoingeckoGateway_GetTokenPrice(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := NewCoingeckoGateay(httpClient.NewHttpClient(true))
+			lgr := logger.NewAppLogger(logger.NewLoggerConfig("debug", true, "console"))
+			lgr.InitLogger()
+			c := NewCoingeckoGateway(lgr, httpClient.NewHttpClient(true))
 			got, err := c.GetTokenPrice(tt.args.tokenIds)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetTokenPrice() error = %v, wantErr %v", err, tt.wantErr)
@@ -79,6 +84,68 @@ func TestCoingeckoGateway_GetTokenPrice(t *testing.T) {
 			for _, tokenId := range tt.args.tokenIds {
 				if _, ok := (*got)[tokenId]; !ok {
 					t.Errorf("GetTokenPrice() required token price for %s not found", tokenId)
+				}
+			}
+		})
+	}
+}
+
+func TestCoingeckoGateway_GetTokenDetails(t *testing.T) {
+	type args struct {
+		tokenId string
+	}
+	tests := []struct {
+		name                string
+		args                args
+		wantPlatformDetails []struct {
+			Platform        string
+			ContractAddress string
+			Decimal         int64
+		}
+		wantErr bool
+	}{
+		{
+			name: "default case, no error expected",
+			args: args{
+				tokenId: "usd-coin",
+			},
+			wantPlatformDetails: []struct {
+				Platform        string
+				ContractAddress string
+				Decimal         int64
+			}{
+				{
+					Platform:        "ethereum",
+					ContractAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+					Decimal:         6,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid token id, error expected",
+			args: args{
+				tokenId: "usd-coin-invalid-xyz",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			lgr := logger.NewAppLogger(logger.NewLoggerConfig("debug", true, "console"))
+			lgr.InitLogger()
+			c := NewCoingeckoGateway(lgr, httpClient.NewHttpClient(true))
+			got, err := c.GetTokenDetails(tt.args.tokenId)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetTokenDetails() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			for _, detail := range tt.wantPlatformDetails {
+				if got.DetailPlatforms[detail.Platform].ContractAddress != detail.ContractAddress {
+					t.Errorf("GetTokenDetails() expected platform contract address %v not found", detail)
+				}
+				if got.DetailPlatforms[detail.Platform].GetDecimalPlace() != detail.Decimal {
+					t.Errorf("GetTokenDetails() expected platform decimal %v not found", detail)
 				}
 			}
 		})
